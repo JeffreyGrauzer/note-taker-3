12ac52d191ada6adf42c949f7f0975ee0c225df4
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-04-09 13:12:47 -0500

    deployed to heroku

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
